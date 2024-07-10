f5afa39c539a214b70fddb8e66c837a422293d19
Shrau1301p <shraup1301@gmail.com>
2024-07-10 09:50:23 +0530

    Basic Structure Added

---
+++ pet-lover-server-side/src/main/java/com/petcommunity/pet_lover_server_side/PetLoverServerSideApplication.java
@@ -0,0 +1,13 @@
+package com.petcommunity.pet_lover_server_side;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLoverServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLoverServerSideApplication.class, args);
+	}
+
+}
---
+++ pet-lover-server-side/src/test/java/com/petcommunity/pet_lover_server_side/PetLoverServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcommunity.pet_lover_server_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLoverServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
