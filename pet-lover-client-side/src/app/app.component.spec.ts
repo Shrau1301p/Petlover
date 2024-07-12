4ba8f908aba074d650da01740b826702e0f9217e
Shrau1301p <shraup1301@gmail.com>
2024-07-12 11:46:41 +0530

    New Client Structure Added

--- pet-lover-client-side/src/app/app.component.spec.ts
+++ pet-lover-client-side/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pet-lover-client-side' title`, () => {
+  it(`should have as title 'pet-lover-client-side'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('pet-lover-client-side');
